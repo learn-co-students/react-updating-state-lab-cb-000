1d64554995543073fc8c4d76471fe224a0538fec
Alex Segers <a.segers.dev@gmail.com>
2018-09-16 23:01:27 +0000

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,25 @@
-// Code DigitalClicker Component Here
+import React from 'react';
+
+class DigitalClicker extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      timesClicked: 0
+    }
+  }
+
+  handleClick = () => {
+    var clicks = 1 + this.state.timesClicked
+    this.setState({
+      timesClicked: clicks
+    })
+  }
+
+  render() {
+    return (
+      <button onClick={handleClick}>Times clicked: {timesClicked}</button>
+    )
+  }
+}
+
+export default DigitalClicker;
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,60 @@
-// Code YouTubeDebugger Component Here
+import React from 'react';
+
+class YouTubeDebugger extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      js {
+        errors: [],
+        user: null,
+        settings: {
+          bitrate: 8,
+          video: {
+            resolution: '1080p'
+          }
+        }
+      }
+    } // state
+  } // constructor
+
+  handleChangeBitrate = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        bitrate: 12
+      }
+    });
+  }
+
+  handleChangeResolution = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        video: {
+          ...this.state.settings.video,
+          resolution: '720p'
+        }
+      },
+    });
+  }
+
+  render() {
+    const bitrate = this.state.js.settings.bitrate;
+    const resolution = this.state.js.settings.video.resolution;
+
+    return (
+      <div>
+        <button className="bitrate" onClick={handleChangeBitrate}>
+          Bitrate: {bitrate}
+        </button>
+        <button className="Resolution" onClick={handleChangeResolution}>
+          Resolution: {resolution}
+        </button>
+      </div>
+    )
+  }
+
+}
+
+export default YouTubeDebugger;
